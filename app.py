fe8824c0af3341016634791cffbb682d17615a57
Sameer Kirwale <109839366+sameerkirwale@users.noreply.github.com>
2022-10-07 14:54:03 +0530

    changed class name

--- app.py
+++ app.py
@@ -20,47 +20,47 @@ from utils.model import ResNet9
 
 # Loading plant disease classification model
 
-disease_classes = ['Apple___Apple_scab',
-                   'Apple___Black_rot',
-                   'Apple___Cedar_apple_rust',
-                   'Apple___healthy',
-                   'Blueberry___healthy',
-                   'Cherry_(including_sour)___Powdery_mildew',
-                   'Cherry_(including_sour)___healthy',
-                   'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
-                   'Corn_(maize)___Common_rust_',
-                   'Corn_(maize)___Northern_Leaf_Blight',
-                   'Corn_(maize)___healthy',
-                   'Grape___Black_rot',
-                   'Grape___Esca_(Black_Measles)',
-                   'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
-                   'Grape___healthy',
-                   'Orange___Haunglongbing_(Citrus_greening)',
-                   'Peach___Bacterial_spot',
-                   'Peach___healthy',
-                   'Pepper,_bell___Bacterial_spot',
-                   'Pepper,_bell___healthy',
-                   'Potato___Early_blight',
-                   'Potato___Late_blight',
-                   'Potato___healthy',
-                   'Raspberry___healthy',
-                   'Soybean___healthy',
-                   'Squash___Powdery_mildew',
-                   'Strawberry___Leaf_scorch',
-                   'Strawberry___healthy',
-                   'Tomato___Bacterial_spot',
-                   'Tomato___Early_blight',
-                   'Tomato___Late_blight',
-                   'Tomato___Leaf_Mold',
-                   'Tomato___Septoria_leaf_spot',
-                   'Tomato___Spider_mites Two-spotted_spider_mite',
-                   'Tomato___Target_Spot',
-                   'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
-                   'Tomato___Tomato_mosaic_virus',
-                   'Tomato___healthy']
+disease_detect_classes = ['Apple___Apple_scab',
+                          'Apple___Black_rot',
+                           'Apple___Cedar_apple_rust',
+                           'Apple___healthy',
+                           'Blueberry___healthy',
+                           'Cherry_(including_sour)___Powdery_mildew',
+                           'Cherry_(including_sour)___healthy',
+                           'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+                           'Corn_(maize)___Common_rust_',
+                           'Corn_(maize)___Northern_Leaf_Blight',
+                           'Corn_(maize)___healthy',
+                           'Grape___Black_rot',
+                           'Grape___Esca_(Black_Measles)',
+                           'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+                           'Grape___healthy',
+                           'Orange___Haunglongbing_(Citrus_greening)',
+                           'Peach___Bacterial_spot',
+                           'Peach___healthy',
+                           'Pepper,_bell___Bacterial_spot',
+                           'Pepper,_bell___healthy',
+                           'Potato___Early_blight',
+                           'Potato___Late_blight',
+                           'Potato___healthy',
+                           'Raspberry___healthy',
+                           'Soybean___healthy',
+                           'Squash___Powdery_mildew',
+                           'Strawberry___Leaf_scorch',
+                           'Strawberry___healthy',
+                           'Tomato___Bacterial_spot',
+                           'Tomato___Early_blight',
+                           'Tomato___Late_blight',
+                           'Tomato___Leaf_Mold',
+                           'Tomato___Septoria_leaf_spot',
+                           'Tomato___Spider_mites Two-spotted_spider_mite',
+                           'Tomato___Target_Spot',
+                           'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+                           'Tomato___Tomato_mosaic_virus',
+                           'Tomato___healthy']
 
 disease_model_path = 'models/plant_disease_model.pth'
-disease_model = ResNet9(3, len(disease_classes))
+disease_model = ResNet9(3, len(disease_detect_classes))
 disease_model.load_state_dict(torch.load(
     disease_model_path, map_location=torch.device('cpu')))
 disease_model.eval()
@@ -119,7 +119,7 @@ def predict_image(img, model=disease_model):
     yb = model(img_u)
     # Pick index with highest probability
     _, preds = torch.max(yb, dim=1)
-    prediction = disease_classes[preds[0].item()]
+    prediction = disease_detect_classes[preds[0].item()]
     # Retrieve the class label
     return prediction
 
